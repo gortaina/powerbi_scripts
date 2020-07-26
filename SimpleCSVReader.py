48207eae594a7f2b99c84467585a67f11b83e736
Joel Melo <arquitetura.joel@gmail.com>
2020-07-26 20:08:28 +0000

    Update SimpleCSVReader.py

--- SimpleCSVReader.py
+++ SimpleCSVReader.py
@@ -10,3 +10,13 @@ for each_list in apps_data[1:]:
     all_ratings.append(rating)
     
 avg_rating = sum(all_ratings)/len(all_ratings)
+
+for each_moma in moma:
+    nationality = each_moma[2]
+    nationality = nationality.replace("(","")
+    nationality = nationality.replace(")","")
+    each_moma[2] = nationality
+    gender  = each_moma[5]                           
+    gender = gender.replace("(","")
+    gender = gender.replace(")","")
+    each_moma[5] = gender  
